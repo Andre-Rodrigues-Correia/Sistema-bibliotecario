77d612bea30950feced41f352201739dccc8a1b0
Jones Brito <jones.brito14@gmail.com>
2021-06-23 19:07:50 -0300

    Jones: View

---
+++ src/sistemaBibliotecario/controller/Tela03_ExemplarController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemaBibliotecario.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author jones
+ */
+public class Tela03_ExemplarController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
